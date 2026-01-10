bd54e542451d9cdfadfb006e20713bff77a068f6
Yauheniya Kavalchuk <y.kavalchuk@godeltech.com>
2026-01-10 03:54:07 +0200

    fix

--- frontend/jest.setup.ts
+++ frontend/jest.setup.ts
@@ -2,18 +2,18 @@ import { TextDecoder, TextEncoder } from 'util';
 import { ReadableStream, WritableStream, TransformStream } from 'stream/web';
 
 Object.assign(globalThis, {
-    TextDecoder,
-    TextEncoder,
-    ReadableStream,
-    WritableStream,
-    TransformStream,
+  TextDecoder,
+  TextEncoder,
+  ReadableStream,
+  WritableStream,
+  TransformStream,
 });
 
 import { fetch, Headers, Request, Response } from 'undici';
 
 Object.assign(globalThis, {
-    fetch,
-    Headers,
-    Request,
-    Response,
+  fetch,
+  Headers,
+  Request,
+  Response,
 });
