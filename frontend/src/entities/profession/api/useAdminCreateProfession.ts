5f570bd245ce3748fefaabba457762e066a6d005
Yauheniya Kavalchuk <y.kavalchuk@godeltech.com>
2026-01-13 22:34:37 +0200

    add professions

--- frontend/src/entities/profession/api/useAdminCreateProfession.ts
+++ frontend/src/entities/profession/api/useAdminCreateProfession.ts
@@ -5,11 +5,13 @@ import { getGetAll2QueryKey, useCreate4 } from '@/shared/api/generated/api';
 
 export function useAdminCreateProfession() {
     const qc = useQueryClient();
+    const key = getGetAll2QueryKey();
 
     return useCreate4({
         mutation: {
-            onSuccess: () => {
-                qc.invalidateQueries({ queryKey: getGetAll2QueryKey() });
+            onSuccess: async () => {
+                await qc.refetchQueries({ queryKey: key, type: 'active' });
+                qc.invalidateQueries({ queryKey: key });
             },
         },
     });
