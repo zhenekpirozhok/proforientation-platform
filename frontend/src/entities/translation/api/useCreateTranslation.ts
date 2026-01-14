f84739efc2ad506acdd6319191d40a53a2295ff5
Yauheniya Kavalchuk <y.kavalchuk@godeltech.com>
2026-01-14 20:43:44 +0200

    add translation hooks

---
+++ frontend/src/entities/translation/api/useCreateTranslation.ts
@@ -0,0 +1,14 @@
+import { useCreate as useCreateGenerated, getSearchQueryKey } from '@/shared/api/generated/api';
+import { useQueryClient } from '@tanstack/react-query';
+
+export const useCreateTranslation = () => {
+    const qc = useQueryClient();
+
+    return useCreateGenerated({
+        mutation: {
+            onSuccess: () => {
+                qc.invalidateQueries({ queryKey: getSearchQueryKey() });
+            },
+        },
+    });
+};
